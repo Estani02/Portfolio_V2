8392a532655197f71b280bf856d9ed7e72266b1d
Estani02 <estaniolmedo@gmail.com>
2023-09-01 18:48:53 -0300

    gif compression

--- src/components/SpotifyCard/SpotifyCard.tsx
+++ src/components/SpotifyCard/SpotifyCard.tsx
@@ -24,15 +24,15 @@ export default function SpotifyCard() {
   }, [data, isLoading])
 
   return (
-    <section className="flex w-full max-w-md flex-col items-center overflow-hidden rounded-2xl bg-[#fffa] p-4 pb-6 backdrop-blur-2xl md:max-w-lg md:flex-col md:items-start">
+    <section className="flex w-full max-w-md flex-col items-center overflow-hidden rounded-2xl bg-white/30 p-4 pb-6 backdrop-blur-sm md:max-w-lg md:flex-col md:items-start">
       <div className="flex flex-col items-center gap-2 md:flex-row md:items-start">
         <Image
           alt={data?.name || 'Song coverpage'}
           className="h-[200px] w-[200px] object-cover"
-          height={data?.album.images[0].height}
+          height={200}
           loading="lazy"
           src={data?.album.images[0].url || noise}
-          width={data?.album.images[0].width}
+          width={200}
         />
         <div className="flex flex-col items-center md:items-start">
           <p className="text-lg font-bold text-[#0000005a]">Recently listened</p>

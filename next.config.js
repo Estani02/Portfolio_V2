12fef4aa6a96415faf1021f3380287abbed1a0f0
Estani02 <estaniolmedo@gmail.com>
2023-09-01 10:55:31 -0300

    added hostname image & new gif | img

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    // domains: ['lirp.cdn-website.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.scdn.co',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig

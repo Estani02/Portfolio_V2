56f57bd91799ce372a93b6de06b6b5cf4a44b828
Estani02 <estaniolmedo@gmail.com>
2023-09-01 18:48:26 -0300

    remove bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import SpotifyCard from '@/components/SpotifyCard/SpotifyCard'
 
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col justify-center bg-slate-800 px-6">
+    <main className="flex h-screen flex-col justify-center px-6">
       <SpotifyCard />
     </main>
   )
